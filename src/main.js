b62ebc238fb6cf49cff28cfcb797a4a69e384260
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-08 11:12:47 +0700

    Fixed Logger

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 import { app } from "./app/app.js";
-import { logger } from "./utils/logging.js";
+import logger from "./utils/logger.js";
 
 app.listen(PORT, logger.info(`App started on port ${PORT}`));
