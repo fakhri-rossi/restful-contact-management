1f0773e43bfd06470b427181f4caa0d71189b166
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-08 09:04:40 +0700

    Update maxlengths

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,16 +5,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Username have to be filled"],
     unique: [true, "Username is already filled"],
-    maxlength: 100,
+    maxlength: [100, "username cannot be more than 100 length"],
   },
   name: {
     type: String,
     required: [true, "Name have to be filled"],
+    maxlength: [100, "Name cannot be more than 100 length"],
   },
   password: {
     type: String,
     required: [true, "Password have to be filled"],
-    minlength: 6,
+    minlength: [6, "Password length cannot be less than 6"],
   },
   token: {
     type: String,
