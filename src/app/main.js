33e461ad05fdd89f5b03f9a328a404adf65d28f0
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-08 09:01:48 +0700

    Restructure Project

--- src/app/main.js
+++ src/app/main.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { connectDB } from "./utils/db.js";
+import { connectDB } from "./db.js";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 connectDB();
 
