70282c980fd0a07649f747c625a0d7935ae015a8
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-08 07:35:54 +0700

    Create User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username have to be filled"],
+    unique: [true, "Username is already filled"],
+    maxlength: 100,
+  },
+  name: {
+    type: String,
+    required: [true, "Name have to be filled"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password have to be filled"],
+    minlength: 6,
+  },
+  token: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
