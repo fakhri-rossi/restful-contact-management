d35de8d0d3b52e05a6e97eb41c9c78288496b105
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-08 11:13:09 +0700

    Set abortEarly: false

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,10 +1,12 @@
 import { ResponseError } from "../error/response-error";
 
 const validate = (schema, request) => {
-  const result = schema.validate(request);
+  const result = schema.validate(request, {
+    abortEarly: false,
+  });
 
   if (result.error) {
-    throw new ResponseError(400, result.error.message)
+    throw new ResponseError(400, result.error.message);
   } else {
     return result.value;
   }
